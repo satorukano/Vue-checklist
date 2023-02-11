4e4f265442b099b8838cde51f9545c9da8cd0e5e
satorukano <kiritomo0206@icloud.com>
2023-02-12 00:43:35 +0900

    [fix] fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "Vue-checklist",
 	devServer:{
         proxy:{
             '^/api':{
